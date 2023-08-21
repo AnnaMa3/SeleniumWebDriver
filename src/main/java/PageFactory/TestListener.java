f73b5be1e0c9eaa9980c6db59b97a4f4930f3bcb
AnnaMa3 <ann.matveyenko@gmail.com>
2023-08-21 15:35:12 +0200

    Commit 12 - Screenshot updated

---
+++ src/main/java/PageFactory/TestListener.java
@@ -0,0 +1,71 @@
+package PageFactory;
+
+import driver.Driver;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class TestListener implements ITestListener {
+    private WebDriver driver;
+    private Logger logger = LoggerFactory.getLogger(TestListener.class);
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+
+    }
+
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        String testName = iTestResult.getMethod().getMethodName();
+        driver = Driver.getDriver();
+
+        Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+        AccountPageFactory.createArtifactsFolder();
+
+        try {
+            File screenshotFile = new File("target/artifacts/"+testName+"failure.png");
+            ImageIO.write(screenshot.getImage(), "PNG", screenshotFile);
+            logger.info("Full-page screenshot is saved due to test failure: "+testName);
+        } catch (IOException e){
+            logger.error("Failed to save");
+        }
+
+        
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
--- src/test/java/ScreenshotTest.java
+++ src/test/java/ScreenshotTest.java
@@ -1,22 +1,30 @@
 import PageFactory.AccountPageFactory;
 import PageFactory.HomePageFactory;
+import PageFactory.TestListener;
 import config.TestProperties;
 import driver.Driver;
 import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.io.FileHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+import javax.imageio.ImageIO;
 
 import java.io.File;
 import java.io.IOException;
 
+
+@Listeners(TestListener.class)
 public class ScreenshotTest extends BaseTest{
     private static final String LOGIN = TestProperties.get("login");
     private static final String PASSWORD = TestProperties.get("password");
 
     private WebDriver driver;
+    private Logger logger = LoggerFactory.getLogger(ScreenshotTest.class);
 
     @Test
     public void screenshotTest(){
@@ -24,15 +32,20 @@ public class ScreenshotTest extends BaseTest{
 
         AccountPageFactory accountPageFactory = new AccountPageFactory(driver);
         Assertions.assertTrue(AccountPageFactory.isDisplayed());
+        AccountPageFactory.createArtifactsFolder();
 
         driver = Driver.getDriver();
-        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+
         try{
-            FileHandler.copy(screenshotFile, new File("C:/Users/AnnaMatveenko/OneDrive - Coherent Solutions/SPARTA/Automation/homepage.png"));
-            System.out.println("Screenshot of Homepage is saved.");
+            AccountPageFactory.createArtifactsFolder();
+            File screenshotFile = new File("target/artifacts/homepage.png");
+            ImageIO.write(screenshot.getImage(), "PNG", screenshotFile);
+            logger.info("Full-page screenshot is saved.");
         } catch (IOException e){
-            e.printStackTrace();
+            logger.error("Failed to save");
         }
+
 
         Assertions.assertEquals(AccountPageFactory.getUserName(), LOGIN);
 
